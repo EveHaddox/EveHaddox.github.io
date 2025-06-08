90c098ef424a443937740098b33aa1a6b46bbe5f
EveHaddox <143825195+EveHaddox@users.noreply.github.com>
2025-06-09 00:07:51 +0200

    update

--- next.config.js
+++ next.config.js
@@ -1,7 +1,4 @@
-// next.config.js
 module.exports = {
-  // Tell Next.js to produce a static export
   output: 'export',
-  // Keep your trailing slashes if you still need them
   trailingSlash: true,
 };
